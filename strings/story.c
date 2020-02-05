2df2042a6b08aecce7d94b483e971d35e7641378
Globe-Eater <bullokl@ostatemail.okstate.edu>
2020-02-04 21:56:33 -0600

    Getting started with C.

---
+++ strings/story.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+
+	char characterName[] = "Kellen";
+	int characterAge = 27;
+	printf("There once was a man named %s\n", characterName);
+	printf("he was %i years old.\n", characterAge);
+	printf("He really liked the name %s\n", characterName);
+	printf("but did not like being %i \n", characterAge);
+
+	return 0;
+}
