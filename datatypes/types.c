a0d3358f0fb7b13a5d324c0e16410d3106a55295
Globe-Eater <bullokl@ostatemail.okstate.edu>
2020-02-09 19:08:26 -0600

    Started learning varaibles and datatypes.

---
+++ datatypes/types.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int count = 5;
+	double pie = 3.14;
+	printf(pie);
+	float magnum = 3.57;
+	printf(magnum);
+	char letter = 'K';
+	printf(letter);
+	char name[] = "Kellen";
+	printf('His name is %s', name);
+
+	return 0;
+}
