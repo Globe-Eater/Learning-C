c4ebacc1cfeedcecda2dc798fb84b708d3762450
Kellen <56308977+Globe-Eater@users.noreply.github.com>
2020-04-13 21:59:14 -0500

    Add files via upload

---
+++ input_output/input_output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int id;
+    printf("Enter your id: ");
+    scanf("%d", &id);
+    printf("Your ID is: %d\n", id); 
+
+    return 0;
+}
