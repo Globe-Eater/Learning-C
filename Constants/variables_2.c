07f226da21001d2dcc01bb4dd51dd11a9c4905d8
Kellen <56308977+Globe-Eater@users.noreply.github.com>
2020-04-13 21:40:02 -0500

    Add files via upload

---
+++ Constants/variables_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* Things go here */ 
+    Const int NUM = 5;
+    printf("%d\n", NUM);
+    NUM = 5 * 2;
+    printf("%d\n", NUM); 
+
+    return 0;
+
+}
